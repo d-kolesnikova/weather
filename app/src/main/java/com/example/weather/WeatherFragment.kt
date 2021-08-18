0f5bb512f86aebe792868b6e23e4b903fddc3396
d-kolesnikova <odarka12@gmail.com>
2021-08-18 17:16:15 +0300

    add data format

--- app/src/main/java/com/example/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/weather/WeatherFragment.kt
@@ -7,11 +7,24 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.weather.data.CurrentWeather
 import com.example.weather.databinding.FragmentWeatherBinding
+import java.text.DateFormatSymbols
+import java.text.SimpleDateFormat
 import java.util.*
 
 class WeatherFragment : Fragment() {
     private var _binding: FragmentWeatherBinding? = null
     private val binding get() = _binding!!
+
+    private fun convertDateToString(time: Date): String {
+        val locale = Locale("ru")
+        val dfs = DateFormatSymbols.getInstance(locale)
+        dfs.shortWeekdays = arrayOf<String>("", "Bс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб")
+        val format = SimpleDateFormat("E, d MMMM", locale)
+        format.dateFormatSymbols = dfs
+        return format.format(time)
+
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,7 +47,7 @@ class WeatherFragment : Fragment() {
             humidity = 33,
         )
         with(binding) {
-            tvDate.text = currentWeather.date.toString()
+            tvDate.text = convertDateToString(currentWeather.date)
             tvTemp.text = currentWeather.tempMin.toString().plus("/").plus(currentWeather.tempMax)
             tvHumidity.text = currentWeather.humidity.toString()
             tvWind.text = currentWeather.windSpeed.toString()
